7822d37aafafe90859da15bb02bb8e88d833790f
Aspcat12 <114385681+Aspcat12@users.noreply.github.com>
2025-01-19 03:22:06 +0700

    Router

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const express = require("express");
 const chalk = require("chalk");
-const app = express();
-const debug = require("debug");
+const debug = require("debug")("app");
 const morgan = require("morgan");
-const port = 3000;
+const path = require("path");
+
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+const productsRouter = require("./src/router/productsRouter.js")
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
+
+app.set("views","./src/views");
+app.set("view engine","ejs");
+
+app.use("/products", productsRouter);
 
 app.get("/", (req, res) => {
 
-    res.send('Hello chat');
+    // res.send('Hello chat');
+    res.render('index',{username: 'Aspcat12',customers: ['kit','Cat','Chat']});
 
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
 
-    console.log("Listening on port %s", chalk.red(port));
+    // console.log("Listening on port %s", chalk.red(port));
+    debug("Listening on port "+ chalk.red(PORT));
 
 });
